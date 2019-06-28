b4a9dee1ba813a09ea8e4357f56b4d6a4d004e53
Saad Awan <msaadawan0@gmail.com>
2019-06-28 18:45:49 +0500

    username corrected

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Results from "./Results";
 
 const App = () => {
@@ -9,8 +9,10 @@ const App = () => {
     <BrowserRouter>
       <div>
         <h1>Adopt me!</h1>
-        <Route path="/" component={SearchParams} />
-        <Route path="/results" component={Results} />
+        <Switch>
+          <Route path="/" component={SearchParams} />
+          <Route path="/results" component={Results} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
